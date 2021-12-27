d82953a4e3a201feb80757610778cd3c691767f0
先規　勇樹 <yukisaki80@gmail.com>
2021-12-27 22:49:52 +0900

    ログインviewを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_075045) do
+ActiveRecord::Schema.define(version: 2021_12_27_084453) do
 
   create_table "users", force: :cascade do |t|
-    t.string "provider", null: false
-    t.string "uid", null: false
-    t.string "name", null: false
-    t.string "image_url", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

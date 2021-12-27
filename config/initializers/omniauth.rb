ffd00f69174259b82d4c65c624b9d9c594291c82
先規　勇樹 <yukisaki80@gmail.com>
2021-12-27 16:52:41 +0900

    userモデルを追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider :github[:client_id], :github[:client_secret]
+    provider :github,
+      ENV['GITHUB_ID'],
+      ENV['GITHUB_SECRET']
+    # provider :github[:client_id], :github[:client_secret]
   else
     provider :github,
       Rails.application.credentials.github[:client_id],

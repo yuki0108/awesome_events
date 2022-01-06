bff708a4fee790e77d4120172ed18dd711091c49
先規　勇樹 <yukisaki80@gmail.com>
2022-01-06 12:28:32 +0900

    modelを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :created_events, class_name: "Event", foreign_key: "owner_id"
+  has_many :tickets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

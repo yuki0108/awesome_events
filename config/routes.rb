604e7c564b05a8623f6c08ccee64a96b2c234f56
先規　勇樹 <yukisaki80@gmail.com>
2022-01-17 20:56:34 +0900

    routeを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :events
   devise_for :users
   root 'welcome#index'
   get "/auth/:provider/callback" => "session#create"
+  resources :events do
+    resources :tickets
+  end
 end
